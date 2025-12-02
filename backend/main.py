48159b2c36aac59348f8997b0cd2e6affafc67bf
Alejandro Martín <alejandrmartnaudn@gmail.com>
2025-12-02 16:39:49 +0100

    Config and database model part 1

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from core.config import settings
+
+app = FastAPI(
+    title="Interactive Stories API",
+    description="API for Interactive Stories",
+    version="0.0.1",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
